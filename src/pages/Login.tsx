189cac29345d0d0ff717e78a5ed0a23db5c00ffd
[dyad] <tamadeu@hotmail.com>
2025-08-17 10:39:51 -0300

    [dyad] Adicionando login com Google - wrote 1 file(s), executed 1 SQL queries

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -37,7 +37,7 @@ const Login = () => {
         <Auth
           supabaseClient={supabase}
           appearance={{ theme: ThemeSupa }}
-          providers={[]}
+          providers={['google']}
           theme="dark"
           localization={{
             variables: {
