170cf621562c7e5ee7bb063b4465f83dd420925e
[dyad] <tamadeu@hotmail.com>
2025-08-17 21:54:23 -0300

    [dyad] Adicionando pull-to-refresh - wrote 6 file(s), added react-pull-to-refresh, @types/react-pull-to-refresh package(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
+import "react-pull-to-refresh/dist/index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
